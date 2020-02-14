1636630c3449ccb67ce5497ed1960fdd77f67119
Danilo <daniloctg@msn.com>
2020-02-14 16:16:27 -0300

    GG

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const path = require('path');
+const app = express();
 
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.use(express.static('files'));
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 
 app.get('/', function (req, res) {
-  res.send('Vinicius Troll man');
+  res.send('SERVER ONLINE');
+});
+
+app.get('/login',function(req,res){
+  res.sendFile(path.join(__dirname+'/html/login.html'));
 });
 
 app.listen(process.env.PORT || 3000, function () {
